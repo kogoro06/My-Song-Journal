2f55de9bd3ff49c87958b731b005f10d2062f599
kogoro06 <166668414+kogoro06@users.noreply.github.com>
2024-11-03 04:13:49 +0900

    設定いじってみる

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -272,7 +272,7 @@ Devise.setup do |config|
   # Add a new OmniAuth provider. Check the wiki for more information on setting
   # up on your models and hooks.
   # config.omniauth :github, 'APP_ID', 'APP_SECRET', scope: 'user,public_repo'
-  config.omniauth :spotify, ENV["SPOTIFY_CLIENT_ID"], ENV["SPOTIFY_CLIENT_SECRET"], scope: "user-read-email user-read-private user-top-read user-read-recently-played playlist-read-private playlist-modify-private playlist-modify-public user-modify-playback-state user-read-playback-state user-read-currently-playing", redirect_uri: Rails.env.production? ? ENV['REDIRECT_URI'] : "http://localhost:3000/auth/spotify/callback"
+  config.omniauth :spotify, ENV["SPOTIFY_CLIENT_ID"], ENV["SPOTIFY_CLIENT_SECRET"], scope: "user-read-email user-read-private user-top-read user-read-recently-played playlist-read-private playlist-modify-private playlist-modify-public user-modify-playback-state user-read-playback-state user-read-currently-playing", redirect_uri: Rails.env.production? ? ENV["REDIRECT_URI"] : "http://localhost:3000/auth/spotify/callback"
 
   # ==> Warden configuration
   # If you want to use other strategies, that are not supported by Devise, or
