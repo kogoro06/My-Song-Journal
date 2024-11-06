13cdc0acc1d3ae09eb835b3441b39b51eb63411f
kogoro06 <166668414+kogoro06@users.noreply.github.com>
2024-11-07 01:19:01 +0900

    プロフィールページとプロフィール編集ページの作成

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,6 @@
 class User < ApplicationRecord
   has_many :journals, dependent: :destroy
+  has_one_attached :profile_image
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
