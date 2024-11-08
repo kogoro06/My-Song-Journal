e1f49f266609861cf81b39a79e1afa205dc90c73
kogoro06 <166668414+kogoro06@users.noreply.github.com>
2024-11-08 19:45:53 +0900

    testなおらん

--- test/controllers/users_controller_test.rb
+++ test/controllers/users_controller_test.rb
@@ -4,12 +4,13 @@ class UsersControllerTest < ActionDispatch::IntegrationTest
   include Devise::Test::IntegrationHelpers  # Deviseのヘルパーをインクルード
 
   setup do
-    @user = users(:one)
-    sign_in @user  # ユーザーをサインイン
+    @user = users(:one)  # users.yml にあるテストユーザーのデータ
+    @user.confirm  # ユーザーがconfirmableを必要とする場合に必要
+    sign_in @user  # ユーザーをサインインさせる
   end
 
   test "should get show" do
-    get user_url(@user)  # 適切なユーザーのURLにアクセス
+    get user_url(@user)  # ユーザー詳細ページのURL
     assert_response :success
   end
 end
