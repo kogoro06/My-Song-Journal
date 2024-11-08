e6db24146140cc11aca0eae35cc520ea1936b7bd
kogoro06 <166668414+kogoro06@users.noreply.github.com>
2024-11-08 18:48:53 +0900

    test修正

--- test/controllers/users_controller_test.rb
+++ test/controllers/users_controller_test.rb
@@ -1,8 +1,5 @@
-require "test_helper"
-
-class UsersControllerTest < ActionDispatch::IntegrationTest
-  test "should get show" do
-    get users_show_url
-    assert_response :success
-  end
+test "should get show" do
+  user = User.create!(email: "unique_email_#{SecureRandom.hex(4)}@example.com", password: "password")
+  get user_url(user)
+  assert_response :success
 end
